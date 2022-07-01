f18b9806a9e69745b1c819804d681ca3f5f32ebf
andrenosonjaya <60030270+andrenosonjaya@users.noreply.github.com>
2022-07-02 02:49:08 +0700

    Update config.php

--- lib/config.php
+++ lib/config.php
@@ -1,7 +1,7 @@
 <?php
 include('func.php');
 include('igfunc.php');
-date_default_timezone_set('Asia/India');
+date_default_timezone_set('Asia/Jakarta');
 error_reporting(0);
 /*
 Jika akun terkena feedback_required tenang, tinggal tunggu 24 jam ntar pulih lagi
@@ -9,9 +9,9 @@ Jika akun terkena feedback_required tenang, tinggal tunggu 24 jam ntar pulih lag
 */
 
 //UBAH BAGIAN INI
-$countTarget    = '200'; //Ambil jumlah akun per target
-$sleep_1        = '7'; //Jeda per view story
-$sleep_2        = '7'; //Jeda per view story 1 akun user
+$countTarget    = '1000'; //Ambil jumlah akun per target
+$sleep_1        = '10'; //Jeda per view story
+$sleep_2        = '10'; //Jeda per view story 1 akun user
 //SAMPAI SINI AJA
 
 $answerFile		= 'storyAnswer.txt'; // FIle komentar mu
